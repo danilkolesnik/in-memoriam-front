af7856e6ea96292fb1c033e9bc2b0db2164d976c
danilkolesnik <kolisnyk.danil@gmail.com>
2024-08-20 19:33:47 +0300

    hotfix: save button

--- src/components/forms/bio/Bio.js
+++ src/components/forms/bio/Bio.js
@@ -1,50 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import styles from './bio.module.scss';
+import React, { useState, useEffect } from "react";
+import styles from "./bio.module.scss";
 import axios from "axios";
-import { API } from 'utils/constants';
+import { API } from "utils/constants";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Bio = ({ userInfo, myUserId }) => {
-  
   const [quote, setQuote] = useState("");
   const [bio, setBio] = useState("");
+  const [isChanged, setIsChanged] = useState(false);
+
+  const handleFieldChange = (value, field) => {
+    if (field === "quote") {
+      setQuote(value);
+      
+    } else {
+      setBio(value);
+    }
+    setIsChanged(true);
+  };
 
   useEffect(() => {
-    setQuote(userInfo?.quote || "");
-    setBio(userInfo?.bio || "");
+    setQuote(userInfo?.quote);
+    setBio(userInfo?.bio);
   }, [userInfo]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const userData = {
-        id: userInfo?.id,
-        bio,
-        quote,
-      };
+      id: userInfo?.id,
+      bio,
+      quote,
+    };
 
     try {
-        
-        const response = await axios.patch(
-          `${API}/users/update-info`,
-          userData,
-          {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem("token")}`,
-              "Content-Type": "application/json",
-            },
-          }
-        );
+      const response = await axios.patch(`${API}/users/update-info`, userData, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-Type": "application/json",
+        },
+      });
 
-        if (response.status === 200) {
-          console.log("Данные успешно обновлены:", response.data);
-          localStorage.setItem("userData", JSON.stringify(response.data));
-        }
-      } catch (error) {
-        toast.error("На жаль, сталася помилка");
+      if (response.status === 200) {
+        toast.success("Успiшно оновлено!");
+        localStorage.setItem("userData", JSON.stringify(response.data));
+        setIsChanged(false);
       }
-  }
+    } catch (error) {
+      toast.error("На жаль, сталася помилка");
+    }
+  };
 
   return (
     <form className={styles.textInfoWrapper} onSubmit={handleSubmit}>
@@ -53,7 +59,7 @@ const Bio = ({ userInfo, myUserId }) => {
           <textarea
             className={styles.textAreaQuote}
             defaultValue={userInfo?.quote}
-            onChange={(e) => setQuote(e.target.value)}
+            onChange={(e) => handleFieldChange(e.target.value, "quote")}
             readOnly={myUserId !== userInfo?.id}
             maxLength="150"
             placeholder="«Його творчість і щедрість духу надихали всіх, хто його знав»"
@@ -62,7 +68,7 @@ const Bio = ({ userInfo, myUserId }) => {
             className={styles.textAreaInfo}
             maxLength="1000"
             defaultValue={userInfo?.bio}
-            onChange={(e) => setBio(e.target.value)}
+            onChange={(e) => handleFieldChange(e.target.value, "bio")}
             readOnly={myUserId !== userInfo?.id}
             placeholder="Назар був відомий своєю добротою і почуттям гумору. Він завжди був готовий підтримати і допомогти близьким у важкі часи. Його друзі згадують, як він організовував затишні вечори у своєму домі, де всі почувалися як удома."
           />
@@ -74,11 +80,12 @@ const Bio = ({ userInfo, myUserId }) => {
         </div>
       )}
 
-      {(quote !== userInfo?.quote || bio !== userInfo?.bio) && (
+      {isChanged && (
         <button className={styles.saveButton} type="submit">
           Зберегти
         </button>
       )}
+
       <ToastContainer
         position="bottom-right"
         autoClose={5000}
@@ -90,6 +97,6 @@ const Bio = ({ userInfo, myUserId }) => {
       />
     </form>
   );
-}
+};
 
 export default Bio;
